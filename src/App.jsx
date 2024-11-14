5c723d9cb782958867ba0522cd4fd5de80061b71
Jordan Oliver <j.a.oliver@wustl.edu>
2024-11-13 19:18:47 -0600

    added xIsNext to Board

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
